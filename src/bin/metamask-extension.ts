ce3ee9dc4bca56431cba6faa810079e9618f7bbf
PeterYinusa <peter.yinusa@consensys.net>
2024-03-18 20:21:44 +0000

    feat: configure metamask installation

--- src/bin/metamask-extension.ts
+++ src/bin/metamask-extension.ts
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
 
-import '../metamask/install';
+import { Command } from 'commander';
+import { installMetaMaskWallet } from '../metamask/install';
+import { PACKAGE_VERSION } from '../constants';
+
+const program = new Command();
+program
+  .version(PACKAGE_VERSION)
+  .description('install wallet browser extensions')
+  .option('-w, --wallet <value>', 'wallet to install for tests')
+  .option('-r, --release <value>', 'release version to download')
+  .option('-c, --channel <value>', 'name of the release channel')
+  .option('-d, --directory <value>', 'path to the download directory');
+
+program.parse(process.argv);
+const { wallet, release, channel, directory } = program.opts();
+
+(async function () {
+  switch (wallet) {
+    case 'metamask':
+      await installMetaMaskWallet(release, channel, directory);
+      break;
+    default:
+      console.error('[ERROR]: Could not find the specified wallet: ' + wallet);
+  }
+})();
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ import pkg from '../../package.json';
 import { Browser, Framework, Wallet } from '../types';
 
 const PACKAGE_NAME: string = pkg.name;
+const PACKAGE_VERSION: string = pkg.version;
 const DEFAULT_CHANNEL: string = 'metamask-chrome';
 const DEFAULT_VERSION: string = '11.10.1';
 const RECOMMENDED_VERSIONS: string = '>=11.10.0 <11.13.0';
@@ -18,6 +19,7 @@ export {
   DEFAULT_VERSION,
   NODE_MODULE_DIR,
   PACKAGE_NAME,
+  PACKAGE_VERSION,
   RECOMMENDED_VERSIONS,
   METAMASK,
   PLAYWRIGHT,
