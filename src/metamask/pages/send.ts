28f6dc2fe5f863fb6f1c4b6c25359838e62100a5
PeterYinusa <peter.yinusa@gmail.com>
2024-11-24 14:25:50 +0000

    feat: update mm send page object

--- src/metamask/pages/send.ts
+++ src/metamask/pages/send.ts
@@ -11,8 +11,8 @@ import { ConfirmTransaction } from './confirm-transaction';
  * @implements {IConfirmation}
  */
 export class Send extends ConfirmTransaction implements IConfirmation {
-  protected nextButton: () => HTMLElement = () => new HTMLElement('[data-testid="page-container-footer-next"]');
-  protected cancelButton: () => HTMLElement = () => new HTMLElement('[data-testid="page-container-footer-cancel"]');
+  protected nextButton: () => HTMLElement = () => new HTMLElement('[data-testid="confirm-footer-button"]');
+  protected cancelButton: () => HTMLElement = () => new HTMLElement('[data-testid="confirm-footer-cancel-button"]');
   /**
    * Creates an instance of Send.
    * @memberof Send
