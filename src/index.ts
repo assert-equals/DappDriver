74414eedfd671bd8259a5fe4fa3889cc73f62146
PeterYinusa <peter.yinusa@gmail.com>
2024-12-10 19:48:23 +0000

    chore: remove deprecated package Ganache

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { DappDriver } from './session/dapp-driver';
-export { Node } from './blockchain';
 export { MockServer } from './mock';
-export { Provider } from './provider';
 export { CheckBox, DropDown, HTMLElement, HTMLElementCollection, InputText, Link, Radio } from './controls';
 export { METAMASK, METAMASK_FLASK, RAINBOW, ZERION, HEADLESS, PLAYWRIGHT, WEBDRIVER, CHROME } from './constants';
 export { PageObject } from './page';
