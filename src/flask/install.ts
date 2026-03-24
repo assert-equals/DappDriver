363013af004b6ad579e6d4ec2895e867a9f2a2cb
PeterYinusa <peter.yinusa@gmail.com>
2026-03-24 21:47:28 +0000

    fix: propergate wallet install errors

--- src/flask/install.ts
+++ src/flask/install.ts
@@ -7,7 +7,7 @@ import {
   METAMASK_GITHUB_API,
   RECOMMENDED_METAMASK_FLASK_VERSIONS
 } from '../constants';
-import { logError, logInfo, logSuccess } from '../log';
+import { logInfo, logSuccess } from '../log';
 import { Asset } from '../types';
 import {
   compareVersion,
@@ -20,26 +20,22 @@ import {
 } from '../wallet/install';
 
 export async function install(directory: string, version: string = DEFAULT_METAMASK_FLASK_VERSION): Promise<string> {
-  try {
-    const assetName = `${DEFAULT_METAMASK_FLASK_ASSET}-${version}-flask.0`;
-    let destDir: string = `${directory}/${assetName}`;
-    const exists = fileExists(destDir);
-    if (exists) {
-      logInfo(`MetaMask Flask version <v${version}> already exists in ${destDir}`);
-      return destDir;
-    }
-    compareVersion(METAMASK_FLASK, version, RECOMMENDED_METAMASK_FLASK_VERSIONS);
-    const release: any = await fetchGithubRelease(METAMASK_FLASK, version, METAMASK_GITHUB_API);
-    const asset: Asset = findGithubAsset(assetName, release);
-    createDirectory(directory);
-    const fileName: string = await downloadAssetZipFile(asset, directory);
-    destDir = extractZipContents(fileName);
-    await enableMetaMaskAutomation(destDir);
-    logSuccess(`Installed MetaMask Flask version <v${version}>\n${destDir}`);
+  const assetName = `${DEFAULT_METAMASK_FLASK_ASSET}-${version}-flask.0`;
+  let destDir: string = `${directory}/${assetName}`;
+  const exists = fileExists(destDir);
+  if (exists) {
+    logInfo(`MetaMask Flask version <v${version}> already exists in ${destDir}`);
     return destDir;
-  } catch (error: any) {
-    logError(error.message);
   }
+  compareVersion(METAMASK_FLASK, version, RECOMMENDED_METAMASK_FLASK_VERSIONS);
+  const release: any = await fetchGithubRelease(METAMASK_FLASK, version, METAMASK_GITHUB_API);
+  const asset: Asset = findGithubAsset(assetName, release);
+  createDirectory(directory);
+  const fileName: string = await downloadAssetZipFile(asset, directory);
+  destDir = extractZipContents(fileName);
+  await enableMetaMaskAutomation(destDir);
+  logSuccess(`Installed MetaMask Flask version <v${version}>\n${destDir}`);
+  return destDir;
 }
 
 export async function enableMetaMaskAutomation(metaMaskPath: string): Promise<void> {
--- src/metamask/install.ts
+++ src/metamask/install.ts
@@ -7,7 +7,7 @@ import {
   METAMASK_GITHUB_API,
   RECOMMENDED_METAMASK_VERSIONS
 } from '../constants';
-import { logError, logInfo, logSuccess } from '../log';
+import { logInfo, logSuccess } from '../log';
 import { Asset } from '../types';
 import {
   compareVersion,
@@ -20,26 +20,22 @@ import {
 } from '../wallet/install';
 
 export async function install(directory: string, version: string = DEFAULT_METAMASK_VERSION): Promise<string> {
-  try {
-    const assetName = `${DEFAULT_METAMASK_ASSET}-${version}`;
-    let destDir: string = `${directory}/${assetName}`;
-    const exists = fileExists(destDir);
-    if (exists) {
-      logInfo(`MetaMask version <v${version}> already exists in ${destDir}`);
-      return destDir;
-    }
-    compareVersion(METAMASK, version, RECOMMENDED_METAMASK_VERSIONS);
-    const release: any = await fetchGithubRelease(METAMASK, version, METAMASK_GITHUB_API);
-    const asset: Asset = findGithubAsset(assetName, release);
-    createDirectory(directory);
-    const fileName: string = await downloadAssetZipFile(asset, directory);
-    destDir = extractZipContents(fileName);
-    await enableMetaMaskAutomation(destDir);
-    logSuccess(`Installed MetaMask version <v${version}>\n${destDir}`);
+  const assetName = `${DEFAULT_METAMASK_ASSET}-${version}`;
+  let destDir: string = `${directory}/${assetName}`;
+  const exists = fileExists(destDir);
+  if (exists) {
+    logInfo(`MetaMask version <v${version}> already exists in ${destDir}`);
     return destDir;
-  } catch (error: any) {
-    logError(error.message);
   }
+  compareVersion(METAMASK, version, RECOMMENDED_METAMASK_VERSIONS);
+  const release: any = await fetchGithubRelease(METAMASK, version, METAMASK_GITHUB_API);
+  const asset: Asset = findGithubAsset(assetName, release);
+  createDirectory(directory);
+  const fileName: string = await downloadAssetZipFile(asset, directory);
+  destDir = extractZipContents(fileName);
+  await enableMetaMaskAutomation(destDir);
+  logSuccess(`Installed MetaMask version <v${version}>\n${destDir}`);
+  return destDir;
 }
 
 export async function enableMetaMaskAutomation(metaMaskPath: string): Promise<void> {
