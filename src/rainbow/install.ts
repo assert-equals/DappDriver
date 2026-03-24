cef28c645ba6069328c815580affc0e326f482c8
Peter Yinusa <53189696+PeterYinusa@users.noreply.github.com>
2026-03-24 21:54:15 +0000

    fix: propergate wallet install errors (#6)

--- src/rainbow/install.ts
+++ src/rainbow/install.ts
@@ -1,5 +1,5 @@
 import { DEFAULT_RAINBOW_VERSION, RAINBOW, RAINBOW_GITHUB_API, RECOMMENDED_RAINBOW_VERSIONS } from '../constants';
-import { logError, logInfo, logSuccess } from '../log';
+import { logInfo, logSuccess } from '../log';
 import { Artifact } from '../types';
 import {
   checkEnvVariable,
@@ -15,27 +15,23 @@ import {
 } from '../wallet/install';
 
 export async function install(directory: string, version: string = DEFAULT_RAINBOW_VERSION): Promise<string> {
-  try {
-    const artifactName = `rainbowbx-chrome-v${version}`;
-    let destDir: string = `${directory}/${artifactName}`;
-    const exists = fileExists(destDir);
-    if (exists) {
-      logInfo(`Rainbow version <v${version}> already exists in ${destDir}`);
-      return destDir;
-    }
-    const workflowName = 'Publish to Chrome (Prod)';
-    checkEnvVariable('GITHUB_TOKEN');
-    compareVersion(RAINBOW, version, RECOMMENDED_RAINBOW_VERSIONS);
-    await fetchGithubTags(RAINBOW, version, RAINBOW_GITHUB_API);
-    const workflow = await fetchGithubWorkflow(workflowName, RAINBOW_GITHUB_API);
-    const run = await fetchGithubRun(version, workflowName, workflow, RAINBOW_GITHUB_API);
-    const artifact: Artifact = await fetchGithubArtifact(artifactName, run, RAINBOW_GITHUB_API);
-    createDirectory(directory);
-    const fileName: string = await downloadArtifactZipFile(artifact, directory);
-    destDir = extractZipContents(fileName);
-    logSuccess(`Installed Rainbow version <v${version}>\n${destDir}`);
+  const artifactName = `rainbowbx-chrome-v${version}`;
+  let destDir: string = `${directory}/${artifactName}`;
+  const exists = fileExists(destDir);
+  if (exists) {
+    logInfo(`Rainbow version <v${version}> already exists in ${destDir}`);
     return destDir;
-  } catch (error: any) {
-    logError(error.message);
   }
+  const workflowName = 'Publish to Chrome (Prod)';
+  checkEnvVariable('GITHUB_TOKEN');
+  compareVersion(RAINBOW, version, RECOMMENDED_RAINBOW_VERSIONS);
+  await fetchGithubTags(RAINBOW, version, RAINBOW_GITHUB_API);
+  const workflow = await fetchGithubWorkflow(workflowName, RAINBOW_GITHUB_API);
+  const run = await fetchGithubRun(version, workflowName, workflow, RAINBOW_GITHUB_API);
+  const artifact: Artifact = await fetchGithubArtifact(artifactName, run, RAINBOW_GITHUB_API);
+  createDirectory(directory);
+  const fileName: string = await downloadArtifactZipFile(artifact, directory);
+  destDir = extractZipContents(fileName);
+  logSuccess(`Installed Rainbow version <v${version}>\n${destDir}`);
+  return destDir;
 }
--- src/zerion/install.ts
+++ src/zerion/install.ts
@@ -5,7 +5,7 @@ import {
   ZERION_GITHUB_API,
   RECOMMENDED_ZERION_VERSIONS
 } from '../constants';
-import { logSuccess, logError, logInfo } from '../log';
+import { logSuccess, logInfo } from '../log';
 import { Asset } from '../types';
 import {
   compareVersion,
@@ -19,24 +19,20 @@ import {
 } from '../wallet/install';
 
 export async function install(directory: string, version: string = DEFAULT_ZERION_VERSION): Promise<string> {
-  try {
-    const assetName = `${DEFAULT_ZERION_ASSET}-v${version}`;
-    let destDir: string = `${directory}/${assetName}`;
-    const exists = fileExists(destDir);
-    if (exists) {
-      logInfo(`Zerion version <v${version}> already exists in ${destDir}`);
-      return destDir;
-    }
-    compareVersion(ZERION, version, RECOMMENDED_ZERION_VERSIONS);
-    const release: any = await fetchGithubRelease(ZERION, version, ZERION_GITHUB_API);
-    const asset: Asset = findGithubAsset(assetName, release);
-    createDirectory(directory);
-    const fileName: string = await downloadAssetZipFile(asset, directory);
-    destDir = extractZipContents(fileName);
-    moveFiles(destDir);
-    logSuccess(`Installed Zerion version <v${version}>\n${destDir}`);
+  const assetName = `${DEFAULT_ZERION_ASSET}-v${version}`;
+  let destDir: string = `${directory}/${assetName}`;
+  const exists = fileExists(destDir);
+  if (exists) {
+    logInfo(`Zerion version <v${version}> already exists in ${destDir}`);
     return destDir;
-  } catch (error: any) {
-    logError(error.message);
   }
+  compareVersion(ZERION, version, RECOMMENDED_ZERION_VERSIONS);
+  const release: any = await fetchGithubRelease(ZERION, version, ZERION_GITHUB_API);
+  const asset: Asset = findGithubAsset(assetName, release);
+  createDirectory(directory);
+  const fileName: string = await downloadAssetZipFile(asset, directory);
+  destDir = extractZipContents(fileName);
+  moveFiles(destDir);
+  logSuccess(`Installed Zerion version <v${version}>\n${destDir}`);
+  return destDir;
 }
